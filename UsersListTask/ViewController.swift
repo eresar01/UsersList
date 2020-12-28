0280231089e2a98c904c0065e84210dac0b5aaae
Yerem Sargsyan <eresar01@gmial.com>
2020-12-29 01:02:54 +0400

    Initial Commit

---
+++ UsersListTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UsersListTask
+//
+//  Created by Yerem Sargsyan on 29.12.20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
